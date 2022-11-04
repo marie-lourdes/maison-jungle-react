bf92f1af237ef79ddc51ccfe38e5dcfc0de1b2c4
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-04 14:09:34 +0100

    organisation file app and create first componet of app.js in index.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from "./Banner"
+import Cart from "./Cart"
+
+function App() {
+    return(<React.Fragment>
+       
+        <Banner/>
+        <main>
+        <Cart/>
+        </main>
+        </React.Fragment>
+    )
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+function Banner() {
+    return(
+        <header>
+        <h1>La maison Jungle</h1>
+        </header>
+        
+    );
+
+}
+
+export default Banner;
