acdbc8ba291aad336316a6f7156e5cf5d27d9d13
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-07 16:01:04 +0100

    add img logo in banner component, transition css

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Banner from "./Banner"
-import Cart from "./Cart"
+import Banner from "./Banner";
+import Cart from "./Cart";
+import "../styles/App.css"
+
 
 function App() {
     return(<React.Fragment>
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,20 @@
 import "../styles/Banner.css"
+import logo from "../assets/logo.png"// import declare la variable logo et importe la valeur de la variable en tant que fichier image
 function Banner() {
+    const title = "la maison jungle";
+    // pas d espace en html
     return(
-        <header>
-        <div className= "title">
-            <h1 style={{
-                 margin:" 30% auto",
-                 color:" white"
-                }}>
-             La maison Jungle</h1>
-        </div>
+        <header className="banner"> 
+            <div className="banner__title">
+                <div className="banner__img-logo">
+                <img src={logo} alt="logo maison jungle"/>
+                </div>
+                <h1 style={{
+                    margin:" 30% auto",
+                    color:" white"
+                    }}>
+                {title}</h1>
+            </div>
         </header>
         
         
