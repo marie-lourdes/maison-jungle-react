a1005b0e331878ca9026281e0d99b865b3e4ca78
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-09 13:13:56 +0100

    dataplantList.js: add light and water data

--- src/datas/dataplantList.js
+++ src/datas/dataplantList.js
@@ -6,52 +6,70 @@ const plantList = [
 		category: 'classique',
 		id: '1ed',
         isBestSale: true,
-		isSpecialOffer: true
+		isSpecialOffer: true,
+		light: 1,
+		water: 2
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
 		id: '2ab',
-        isBestSale: true
+        isBestSale: true,
+		light: 2,
+		water: 3
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
-        isBestSale: false
+        isBestSale: false,
+		light: 1,
+		water: 2
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
 		id: '4kk',
         isBestSale: false,
-		isSpecialOffer: true
+		isSpecialOffer: true,
+		light: 3,
+		water: 2
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
-		id: '5pl'
+		id: '5pl',
+		light: 2,
+		water: 2
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
-		id: '6uo'
+		id: '6uo',
+		light: 3,
+		water: 1
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
-		id: '7ie'
+		id: '7ie',
+		light: 3,
+		water: 3
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
 		id: '8fp',
-		isSpecialOffer: true
+		isSpecialOffer: true,
+		light: 1,
+		water: 2
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
-		id: '9vn'
+		id: '9vn',
+		light: 1,
+		water: 1
 	}
 ]
 
