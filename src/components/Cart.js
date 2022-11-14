2b95d5800e2f2990f2545e7628ebda01e433c3ac
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-14 11:08:30 +0100

    cart.js: add useState state cart updateCart for total price monsteras

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,21 +1,22 @@
 import React from "react"
+import {useState} from "react"
 import FormCart from "./FormCart"
 import "../styles/Cart.css"
 function Cart() {
     const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
+    const [cart, updateCart] = useState(0);
+ 
     return (
     <div  className ="cart">
       <div>
           <h2>Panier</h2>
           <ul>
-          <li>Monstera : {monsteraPrice}€</li>
-          <li>Lierre : {ivyPrice}€</li>
-          <li>Fleurs : {flowerPrice}€</li>
+            <li>Monstera : {monsteraPrice}€</li>  
           </ul>
-            Total : {monsteraPrice + ivyPrice + flowerPrice }€
+        <button onClick={()=> updateCart(cart +1)}>Ajouter</button>
       </div>
+      {/* calcul du total avec la valeur actuel de sate cart enregistré par la fonction setState (fonction setState(comprenant la valeur actuel en parametre) valeur de retour de useState) updateCart*/}
+      <h3>Total : {monsteraPrice*cart}€</h3>
       <FormCart/>
     </div>
     )
