6c32ccc0bf58e10524460caef69a5c7d52df2ee1
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-11-17 21:40:32 +0100

    finish exo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import Banner from "./Banner";
 import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
@@ -9,14 +9,18 @@ import "../styles/App.css";
 
 function App() {
  
-    //State composant cart avec valeur initial tableau vide pour le nombre d element ajouté au panier
-     const [cart, updateCart] = useState([]);
+    
      const [categoryActive, setCategoryActive] = useState("");
      //state pour cacher le footer et nettoyer les effets dans footer.js 
     const [isFooterShown, setFooterShown] = useState(true)
-
-  
- 
+	//State composant cart avec valeur initial tableau vide si le localstorage est vide ne contient pas de plante enregistré pour le nombre d element ajouté au panier
+	//si le localstorage contient des valeur dans le "cart" on parse et on l attribue à la valeur initial de useState
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {// enregistre a chaque mise a jour du State cart de app.js, mis a jour par shoppingList qui a recupéré la fonction setState de app.s dans la prop, et cart.js le composant recupere la valeur de state cart dans ses props pour afficher l ajout de shoppingList
+		//enregistre apres le re render de app er la mise a jour de state cart de apps.js
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
     return(<React.Fragment>
        
         <Banner/>
